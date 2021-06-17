3e04fde2c76161923e62223a3ce98f36b262e736
Brad Ago <bradnorbertago@gmail.com>
2021-06-17 21:29:55 +0800

    CustomerWebApi

---
+++ Models/CustomerDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICustomer.Models
+{
+    public class CustomerDBContext :DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public CustomerDBContext(DbContextOptions<CustomerDBContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
